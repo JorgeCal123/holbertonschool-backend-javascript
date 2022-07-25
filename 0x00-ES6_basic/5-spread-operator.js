29661dce649dd0a7c704ec03b38687633a5b4e5f
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-25 16:44:00 -0500

    task 5 corr 2

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export function concatArrays(array1, array2, string) {
-    return [...array1, ...array2, ...string];
-  }
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}

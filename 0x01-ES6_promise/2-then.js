9be4e29747958c44064d9eb166ba386278edb9fd
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-27 15:25:37 -0500

    task 2 corr 1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,21 +1,14 @@
-//export default function handleResponseFromAPI (promise) {
-//    const newpromise = () => {
-//        promise.resolve({ status: 200, body: 'Success' });
-//        promise.reject(new Error('The fake API is not working currently'));
-//        promise.finally(() => {('Got a response from the API')});
-//    }
-//    return newpromise;
-//}
+
 
 export default function handleResponseFromAPI (promise) {
     return promise
         .then(() => (
             { status: 200, body: 'Success' }
         ))
-        .catch(() => (
+        .catch(() => 
             new Error('The fake API is not working currently')
-        ))
-        .finally(() => (
-            "Got a response from the API"
-        ));
+        )
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
 };

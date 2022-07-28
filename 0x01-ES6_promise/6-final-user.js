806e5c93a4fc4cea9af0ae87574535c6c3238120
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-27 23:51:45 -0500

    takjdnksdja

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const SUU = signUpUser(firstName, lastName);
   const UP = uploadPhoto(fileName);
 
-    return Promise.allSettled([SUU, UP])
+  return Promise.allSettled([SUU, UP])
     .then((result) => {
-        const list = result.map((value) => {
-          if (value.status === 'fulfilled') {
-            return value; }
-          return { status: value.status, value: `${value.reason}` };
-        });
-        return list;
-      });}
+      const list = result.map((value) => {
+        if (value.status === 'fulfilled') {
+          return value; 
+        }
+        return { status: value.status, value: `${value.reason}` };
+      });
+      return list;
+  });
+}

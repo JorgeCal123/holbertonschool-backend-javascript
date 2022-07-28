ac61a63bf2fe13d22652acb39de9fa100a941b47
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-28 16:35:12 -0500

    task 2 corr 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,10 +5,10 @@ export default class HolbertonCourse {
       throw new TypeError('Name must be a string');
     }
     if (typeof lenght !== 'number') {
-      throw new TypeError('lenght must be a number');
+      throw new TypeError('Lenght must be a number');
     }
     if (Array.isArray(students) === false) {
-      throw new TypeError('students must be a array');
+      throw new TypeError('Students must be a array');
     }
     this._name = name;
     this._lenght = lenght;

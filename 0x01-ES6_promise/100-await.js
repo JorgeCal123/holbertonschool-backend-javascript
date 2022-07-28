d8ad64262d2752054df7166d5bd2806b9c3ba625
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-27 22:28:26 -0500

    task 100 corr 3

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
 
-  let info = {photo: null, user: null};
+  let info = { photo: null, user: null };
 
   try {
     info.photo = await uploadPhoto();

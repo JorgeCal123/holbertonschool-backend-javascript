f054f37993ad3983b07de2987b37655531d545e8
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-27 20:10:00 -0500

    task 6 corr 2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-//import { uploadPhoto, createUser } from './utils';
+//import signUpUser from './4-user-promise';
+//import uploadPhoto from './5-photo-reject';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    return Promise.allSettled([uploadPhoto(fileName), createUser(firstName, lastName)])
     .then((result) => {
         const list2 = []
-        result.map((values) => {
-            list2.push({value: values.value,});
-        })
-        return list2;
-
+            result.map((values) => {
+                list2.push({status: values.status, value: values.value,});
+            })
+            console.log(list2);
+            return list2;
     })
 }

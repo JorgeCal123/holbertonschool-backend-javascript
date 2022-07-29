7a03bf65faba68b2e931a3693365833bd045c6b6
JorgeCal123 <ingsistemas68@gmail.com>
2022-07-28 19:09:30 -0500

    task 4 carr 2

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -10,7 +10,7 @@ export default class Pricing {
    return `${this._amount} ${this._currency._name} (${this._currency._code})`; 
   }
 
-  convertPrice (amount, conversionRate) {
+  static convertPrice (amount, conversionRate) {
     return (amount * conversionRate);
   }
   get amount() {

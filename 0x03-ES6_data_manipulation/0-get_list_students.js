38c6b6b2e3338c25d5f4d499203a93f71f0cbe43
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-01 09:23:44 -0500

    first commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const est1 = {id: 1, firstNme: 'Guillaume', location: 'San Francisco'};  
+  const est2 = {id: 2, firstNme: 'James', location: 'Columbia'};  
+  const est3 = {id: 3, firstNme: 'Serena', location:'San Francisco'};  
+
+  return [est1, est2, est3];
+}

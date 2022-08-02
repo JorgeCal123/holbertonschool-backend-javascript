83df9f81ab8a9397b07eff434f67fd03ffa02138
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-02 10:53:50 -0500

    task 5 corr 1

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,32 @@
+//export default function updateStudentGradeByCity(list, city, newgrades) {
+//  const liststudents = list.filter((student) => student.location === city);
+//  const studentGrade = [];
+//  let grade;
+//  liststudents.map((studentCity) => {
+//    const grades = (newgrades.filter((obj) => obj.studentId === studentCity.id));
+//    if (grades.length === 0) {
+//      grade = 'N/A';
+//    } else {
+//      grade = grades[0].grade;
+//    }
+//    studentGrade.push({ ...studentCity, grade });
+//  });
+//  return studentGrade;
+//}
+
 export default function updateStudentGradeByCity(list, city, newgrades) {
-  const liststudents = list.filter(student => student.location == city);
+  const liststudents = list.filter((student) => student.location === city);
   const studentGrade = [];
   let grade;
-  liststudents.map((studentCity)=> {
-    const grades = (newgrades.filter(obj => obj.studentId == studentCity.id));
+  const gr = liststudents.map((studentCity) => {
+    const grades = (newgrades.filter((obj) => obj.studentId === studentCity.id));
     if (grades.length === 0) {
       grade = 'N/A';
-    }
-    else {
+    } else {
       grade = grades[0].grade;
     }
-    studentGrade.push({...studentCity, grade})
+    return grade;
   });
+  studentGrade.push();
   return studentGrade;
 }
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,5 @@ export default function createInt8TypedArray(lenght, position, value) {
   const int8Array = new Uint8Array(buffer);
   int8Array[position] = value;
   const dataView = new DataView(buffer);
-
   return dataView;
 }

d10651a8fc8aa5993ae4be06f630f2ef12b605c1
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-02 09:01:30 -0500

    task 2 corr 1

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,7 @@
 export default function getStudentsByLocation(list, city) {
+  if (Array.isArray(list)) {
   const result = list.filter((student) => student.location === city);
   return result;
+  }
+  return [];
 }

116264adef4166e57833cf57821555c196aa6e2f
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-02 09:24:22 -0500

    task 3 corr 1

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,8 @@
 export default function getStudentIdsSum(list) {
   let suma = 0;
-  list.map((student)=> {
-    suma = suma + student.id;
+  list.map((student) => {
+    suma += student.id;
+    return suma;
   });
   return suma;
 }

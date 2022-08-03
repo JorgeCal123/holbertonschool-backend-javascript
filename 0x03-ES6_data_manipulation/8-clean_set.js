f723c506b4cfa1d9e65a435a2af1d34256d3e995
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-02 19:49:20 -0500

    task 8 corr 3

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,25 @@
 export default function cleanSet(set, startString) {
-    console.log();
-    return "hola"
+  const list = [...set.values()];
+  if (startString === undefined || startString === '') {
+    return '';
+  }
+  const newlist = [];
+
+  list.forEach((word) => {
+    let word2 = '';
+    if (word !== undefined) {
+      const arr = Array.from(word, (val, index) => {
+        if (word.startsWith(startString)) {
+          if (index >= startString.length) {
+            word2 += val;
+          }
+        }
+        return word2;
+      });
+      if (arr[arr.length - 1] !== '') {
+        newlist.push(arr[arr.length - 1]);
+      }
+    }
+  });
+  return [...newlist].join('-');
 }

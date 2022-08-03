1e075e63d69c0be7e99f5cba32522eab5b833368
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-02 23:27:34 -0500

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,3 @@
-//export default function updateStudentGradeByCity(list, city, newgrades) {
-//  const liststudents = list.filter((student) => student.location === city);
-//  const studentGrade = [];
-//  let grade;
-//  liststudents.map((studentCity) => {
-//    const grades = (newgrades.filter((obj) => obj.studentId === studentCity.id));
-//    if (grades.length === 0) {
-//      grade = 'N/A';
-//    } else {
-//      grade = grades[0].grade;
-//    }
-//    studentGrade.push({ ...studentCity, grade });
-//  });
-//  return studentGrade;
-//}
-
 export default function updateStudentGradeByCity(list, city, newgrades) {
   const liststudents = list.filter((student) => student.location === city);
   const studentGrade = [];
@@ -26,6 +10,7 @@ export default function updateStudentGradeByCity(list, city, newgrades) {
       grade = grades[0].grade;
     }
     studentGrade.push({ ...studentCity, grade });
+    return studentGrade;
   });
   return studentGrade;
 }

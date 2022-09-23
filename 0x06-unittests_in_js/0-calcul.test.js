c3bdd82858cd2ed4eb323aafa658ab5ff9f7ee6d
JorgeCal123 <ingsistemas68@gmail.com>
2022-09-22 22:51:53 -0500

    all task

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,11 +3,11 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('output', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1, -1), 0);
+    assert.equal(calculateNumber(-1, -1), -2);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
   });
 });

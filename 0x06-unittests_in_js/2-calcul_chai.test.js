ff71ee9f087256a7d0b017e3cf586f4af75946ff
JorgeCal123 <ingsistemas68@gmail.com>
2022-09-22 19:34:13 -0500

    all task

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+
+describe('calculateNumber: ', function () {
+	describe('output sum', function () {
+		it('SUM', function () {
+			expect(calculateNumber("SUM", 1, 1)).to.equal(2);
+			expect(calculateNumber("SUM", 1, 1.2)).to.equal(2);
+			expect(calculateNumber("SUM", 1.3, 1)).to.equal(2);
+			expect(calculateNumber("SUM", 1.7, 1)).to.equal(3);
+			expect(calculateNumber("SUM", -1.3, 1.3)).to.equal(0);
+			expect(calculateNumber("SUM", -1.3, -1.3)).to.equal(-2);
+		});
+	});
+	describe("output subtract", function () {
+		it('SUBTRACT', function() {
+			expect(calculateNumber("SUBTRACT", 1.3, 1.8)).to.equal(-1);
+			expect(calculateNumber("SUBTRACT", 1.3, 1.3)).to.equal(0);
+			expect(calculateNumber("SUBTRACT", 1.3, -1.3)).to.equal(2);
+			expect(calculateNumber("SUBTRACT", -1.3, 1.3)).to.equal(-2);
+			expect(calculateNumber("SUBTRACT", -1.3, -1.3)).to.equal(0);
+			expect(calculateNumber("SUBTRACT", 0.2, 0.4)).to.equal(0);
+		});
+	});
+	describe("Divide the first value whit the second", function () {
+		it('DIVIDE', function() {
+			expect(calculateNumber("DIVIDE", 1, 1)).to.equal(1);
+			expect(calculateNumber("DIVIDE", 1.7, 1.2)).to.equal(2);
+			expect(calculateNumber("DIVIDE", 1.3, 1.8)).to.equal(0.5);
+			expect(calculateNumber("DIVIDE", 4.3, 1.8)).to.equal(2);
+			expect(calculateNumber("DIVIDE", 1.3, 1.3)).to.equal(1);
+			expect(calculateNumber("DIVIDE", -1.3, 1.2)).to.equal(-1);
+			expect(calculateNumber("DIVIDE", 1.3, 0)).to.equal("Error");
+			expect(calculateNumber("DIVIDE", -1.3, 0)).to.equal("Error");
+			expect(calculateNumber("DIVIDE", 0, 1)).to.equal(0);
+		});
+	});
+});
